f781098682ed7232bb215272f297d512be0238a1
oluwaseunisrael3-boop <oluwaseunisrael3@gmail.com>
2026-05-21 00:20:10 +0100

    Create theme.dart

---
+++ theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GozomTheme {
+  static const Color deepBlue = Color(0xFF0A192F);
+  static const Color darkBg = Color(0xFF020C1B);
+  static const Color cardBg = Color(0xFF112240);
+  static const Color primaryGold = Color(0xFFF1C40F);
+  static const Color textWhite = Color(0xFFE6F1FF);
+  static const Color textMuted = Color(0xFF8892B0);
+}
